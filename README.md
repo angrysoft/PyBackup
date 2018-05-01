49fe50978d4366555b266ebd6e0447b4297dba90
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2018-05-01 22:30:08 +0200

    init

---
+++ README.md
@@ -0,0 +1,72 @@
+# Backaup
+ Simple backup solution for backuping directory and databases.
+## Configs
+ Configs files are json files for example:
+ ```json
+{
+  "type": "dir",
+  "path": "/etc",
+  "backup_dir": "/home/user/backup",
+  "name": "etc",
+  "compression": "gz"
+}
+```
+## type
+#### dir - Create a new tar archive with directory as source
+* path - source directory path
+* name - archive name
+* backup_dir - output path for archive
+* compression - compression type
+    * gz   - gzip compression
+    * bz2  - bzip compression
+    * xz   - lzma compression
+    * none - without compression
+```json
+{
+  "type": "dir",
+  "path": "/etc",
+  "backup_dir": "/home/user/backup",
+  "name": "etc",
+  "compression": "gz"
+}
+```
+
+#### files - Create a new tar archive with file list as source
+* path - file list 
+* backup_dir - output path for archive
+* compression - compression type
+    * gz   - gzip compression
+    * bz2  - bzip compression
+    * xz   - lzma compression
+    * none - without compression
+```json
+{
+  "type": "dir",
+  "path": ["/etc/locale.conf", "/etc/vconsole.conf"],
+  "backup_dir": "/home/user/backup",
+  "name": "etc",
+  "compression": "gz"
+}
+```
+
+#### mariadb - create database dump
+* dbname - database name when name is "*" dumps all databases 
+* user - db user name
+* password - db password
+* backup_dir - output path for 
+* compression - compression type
+    * gz   - gzip compression
+    * bz2  - bzip compression
+    * xz   - lzma compression
+    * none - without compression
+```json
+{
+  "type": "mariadb",
+  "dbname": "",
+  "user" : "foo",
+  "password": "foobar",
+  "backup_dir": "/home/user/backup",
+  "name": "etc",
+  "compression": "gz"
+}
+```
---
+++ backup.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python3
+
+import signal
+import argparse
+import json
+import sys
+import os
+import tarfile
+import datetime
+import subprocess
+import gzip
+import bz2
+import lzma
+
+
+class Backup:
+    def __init__(self, config):
+        self.config = config
+        self.compressionTypes = ['gz', 'bz2', 'xz']
+
+    def run(self):
+        print(self.config)
+        configPath = self.config.dir
+        if os.path.isdir(configPath):
+            if self.config.config:
+                self.parseConfig((os.path.join(configPath, self.config.config)))
+            else:
+                for c in os.listdir(configPath):
+                    self.parseConfig(os.path.join(configPath, c))
+
+    def parseConfig(self, configFile):
+        with open(configFile, 'r') as jconfig:
+            conf = json.load(jconfig)
+            t = conf.get('type')
+            if t == 'dir':
+                self._backupDir(conf)
+                pass
+            elif t == 'mariadb':
+                if conf.get('mode') == 'dump':
+                    self._backupMariadbDump(conf)
+                elif conf.get('mode') == 'json':
+                    self._backupMariadbJson(conf)
+
+    def _backupDir(self, conf):
+        compression = self._getCompressionType(conf.get('compression'))
+        name = os.path.join(conf.get('backup_dir', '.'),
+                            '{}_{}'.format(conf.get('name'), self._getTimeStamp()))
+        self._info('Backup directory {}'.format(conf.get('path')))
+
+        with tarfile.open(name=name,
+                          mode='w:{}'.format(compression)) as tar:
+            if os.path.exists(conf.get('path')):
+                tar.add(conf.get('path'))
+                tar.close()
+            else:
+                sys.stderr.write('source not exits : {} : SKIPPIN\n'.format(conf.get('path')))
+
+    def _backupMariadbDump(self, conf):
+        args = ['mysqldump']
+        name = 'mariadb'
+        if conf.get('dbname') == '*' or not conf.get('dbname'):
+            args.append('--all-databases')
+        else:
+            args.append(conf.get('dbname'))
+            name = conf.get('dbname')
+        args.append('-u')
+        args.append(conf.get('user', 'root'))
+        args.append('--password={}'.format(conf.get('password', '')))
+
+        ret = subprocess.run(args, stdout=subprocess.PIPE)
+        if ret.returncode == 0:
+            self._saveToFile(conf.get('backup_dir', '.'),
+                             name,
+                             ret.stdout,
+                             suffix='.sql',
+                             compression=conf.get('compression'))
+
+    def _backupMariadbJson(self, conf):
+        print('dumping json {}'.format(conf.get('dbname')))
+
+    def _saveToFile(self, path, name, data, suffix='', compression=None):
+        output = os.path.join(path, '{}_{}{}'.format(name, self._getTimeStamp(), suffix))
+        self._info('Saving file {}.gz'.format(output))
+        if not type(data) == bytes:
+            data = data.encode()
+
+        if compression == 'gz':
+            with gzip.open('{}.gz'.format(output), 'wb') as gzFile:
+                gzFile.write(data)
+        elif compression == 'bz2':
+            with bz2.open('{}.bz2'.format(output), 'wb') as bzFile:
+                bzFile.write(data)
+        elif compression == 'xz':
+            with lzma.open('{}.gz'.format(output), 'wb') as xzFile:
+                xzFile.write(data)
+        else:
+            with open(output, 'wb') as oFile:
+                oFile.write(data)
+
+    def _getCompressionType(self, compression):
+        if compression in self.compressionTypes:
+            return compression
+        else:
+            return ''
+
+    def _info(self, msg):
+        if self.config.verbose:
+            print('>> {}'.format(msg))
+
+    @staticmethod
+    def _getTimeStamp():
+        return datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+
+
+def stop_program(signal, frame):
+    """stop_program"""
+    print('\nExit Program')
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, stop_program)
+    parser = argparse.ArgumentParser(usage='%(prog)s [options]')
+    parser.add_argument('-v', '--verbose', action="store_true", help="verbose")
+    parser.add_argument('-d', '--dir', type=str, default='/etc/PyBackup/',
+                        help='Directory path for config files')
+    parser.add_argument('-c', '--config', default='', type=str,
+                        help='Config file')
+
+    b = Backup(parser.parse_args())
+    b.run()
